a032556bbac785f3df392836e05eb76d74af4d67
ganadenjameel <103278077+ganadenjameel@users.noreply.github.com>
2022-04-28 23:38:43 -0700

    Test File and jar Files

--- cseDemo.java
+++ cseDemo.java
@@ -1,2 +1,5 @@
 public class cseDemo{
+    public static int subtract(int a, int b){
+        return a-b;
+    }
 }
---
+++ cseDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class cseDemoTest {
+    @Test
+    public void subtraction(){
+        assertEquals(2, cseDemo.subtract(4,3));
+    }
+}
